5ba243b6bb1f426079b3d0519fddafd111f6b5fe
Miroslav Ivanov <miroslav.ivanov@hotmail.com>
2016-12-01 14:37:32 +0200

    Added IDataContext.cs and MainWindowDataContext.cs

---
+++ ImagesPathsExtractor/Extractor.WpfClient/Contracts/IDataContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Extractor.WpfClient.Contracts
+{
+    using System.ComponentModel;
+
+    public interface IDataContext : INotifyPropertyChanged
+    {
+        void OnPropertyChanged(string propertyName);
+    }
+}
---
+++ ImagesPathsExtractor/Extractor.WpfClient/MainWindowDataContext.cs
@@ -0,0 +1,15 @@
+﻿namespace Extractor.WpfClient
+{
+    using System.ComponentModel;
+    using Contracts;
+
+    public class MainWindowDataContex : IDataContext, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
